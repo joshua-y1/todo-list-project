0d425e0d963d2df0744ae9f271382abc22bc3b28
Josh Yi <joshua.yi1018@gmail.com>
2026-09-08 22:10:32 -0400

    Connect Express app to MongoDB

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const MongoClient = require('mongodb').MongoClient;
 const PORT = 8888;
 require('dotenv').config();
 
+let db,
+  dbConnectionStr = process.env.DB_STRING,
+  dbName = 'todo';
+
+MongoClient.connect(dbConnectionStr)
+  .then(client => {
+    console.log(`Connected to ${dbName} Database`);
+    db = client.db(dbName);
+  })
+  .catch(err => console.error(`Connection error: ${err}`))
+
 app.get('/', (req, res) => {
     res.send("Hello World");
 })
