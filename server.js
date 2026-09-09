806b9b9c1e9d108d51f948829e9a689ee4ee6e83
Josh Yi <joshua.yi1018@gmail.com>
2026-09-09 00:12:49 -0400

    Add GET route to fetch and render todos

--- server.js
+++ server.js
@@ -18,12 +18,13 @@ MongoClient.connect(dbConnectionStr)
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    res.send("Hello World");
-})
+app.get('/', async (request, response) => {
+  const todoItems = await db.collection('todos').find().toArray();
+  const itemsLeft = await db.collection('todos').countDocuments({completed: false});
+  response.render('index.ejs', { items: todoItems, left: itemsLeft });
+});
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on port ${PORT}`);
